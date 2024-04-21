f8114a5e259465849546e82698103729146b3f74
@arxlan99 <ozkan.arslan.321@gmail.com>
2024-04-21 11:58:25 +0300

    add dummy data

--- db.js
+++ db.js
@@ -1,11 +1,7 @@
-const items = require('./data/items');
-const tags = require('./data/tags');
-const brands = require('./data/brands');
-const itemTypes = require('./data/itemTypes');
+const databases = require('./data/databases');
+const users = require('./data/users');
 
 module.exports = {
-  items,
-  tags,
-  brands,
-  itemTypes,
+  databases,
+  users,
 };
