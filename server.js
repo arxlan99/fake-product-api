e6bd38c8750a2564a516d5dbc86a31a988277260
@arxlan99 <ozkan.arslan.321@gmail.com>
2024-04-21 11:10:49 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const data = require('./db.js');
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(data);
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 5000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+  console.log(`JSON Server is running on port http://localhost:${port}`);
+});
