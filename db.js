4810a721bcdf6734aa5c0b00c3092e6c74cae3e9
@arxlan99 <ozkan.arslan.321@gmail.com>
2024-04-21 13:57:21 +0300

    add cities to db

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const databases = require('./data/databases');
 const users = require('./data/users');
+const cities = require('./data/cities');
 
 module.exports = {
   databases,
   users,
+  cities,
 };
