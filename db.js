4230b4282dca8ebf11b1f92313d1139bf3ffb810
@arxlan99 <ozkan.arslan.321@gmail.com>
2024-04-23 11:07:20 +0300

    publish table and db json

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
-const databases = require('./data/databases');
+const databasesAndTables = require('./data/databasesAndTables');
 const users = require('./data/users');
 const cities = require('./data/cities');
+const databases = require('./data/databases');
 
 module.exports = {
-  databases,
+  databasesAndTables,
   users,
   cities,
+  databases,
 };
