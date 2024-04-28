d1e419638f47c6f2a7eb518048a2da2b1af6458b
@arxlan99 <ozkan.arslan.321@gmail.com>
2024-04-28 11:35:18 +0300

    add auth data

--- db.js
+++ db.js
@@ -2,10 +2,12 @@ const databasesAndTables = require('./data/databasesAndTables');
 const users = require('./data/users');
 const cities = require('./data/cities');
 const databases = require('./data/databases');
+const auth = require('./data/auth');
 
 module.exports = {
   databasesAndTables,
   users,
   cities,
   databases,
+  auth,
 };
